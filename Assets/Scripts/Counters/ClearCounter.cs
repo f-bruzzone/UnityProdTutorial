9372dd0a0a99f849b8abb86f5f111772099cfae1
Frank Bruzzone <fb88@njit.edu>
2023-05-28 02:25:27 -0400

    added ability to add ingredients to plate from player hands to plate or vice versa

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -10,23 +10,48 @@ public class ClearCounter : BaseCounter
     {
         if (!HasKitchenObject())
         {
+            // there is no kitchen object on this counter
             if (player.HasKitchenObject())
             {
+                // player is carrying something
                 player.GetKitchenObject().SetKitchenObjectParent(this);
             }
             else
             {
+                // player is not carrying anything
 
             }
         }
         else
         {
+            // there is a kitchen object on this counter
             if (player.HasKitchenObject())
             {
-
+                // player is carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    // player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    {
+                        GetKitchenObject().DestroySelf();
+                    }
+                }
+                else
+                {
+                    // player is holding something other than a plate
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject))
+                    {
+                        // counter is holding a plate
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO()))
+                        {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
             }
             else
             {
+                // player is not holding anything
                 GetKitchenObject().SetKitchenObjectParent(player);
             }
         }
